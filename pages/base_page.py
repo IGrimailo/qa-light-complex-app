d749664af54483e66f5dd30548e613bfff7a2204
IGrimaylo <igrimailo@corp.sendios.io>
2022-05-31 20:39:20 +0200

    Homework 16.05

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,16 +1,36 @@
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
+        self.waiter = WebDriverWait(driver=driver, timeout=5)
+
+    def wait_until_clickable(self, xpath):
+        """Wait until element is clickable"""
+        return self.waiter.until(method=expected_conditions.element_to_be_clickable((By.XPATH, xpath)))
+
+    def wait_until_displayed(self, xpath):
+        """Wait until element is displayed"""
+        return self.waiter.until(method=expected_conditions.visibility_of_element_located((By.XPATH, xpath)))
+
+    def is_element_exists(self, xpath):
+        """Check if element exists"""
+        try:
+            self.driver.find_element(by=By.XPATH, value=xpath)
+            return True
+        except (TimeoutError, NoSuchElementException):
+            return False
 
     def fill_field(self, xpath, value):
         """Send data into the field"""
-        field = self.driver.find_element(by=By.XPATH, value=xpath)
+        field = self.wait_until_clickable(xpath)
         field.clear()
         field.send_keys(value)
 
     def click(self, xpath):
         """Find and click the element"""
-        self.driver.find_element(by=By.XPATH, value=xpath).click()
+        self.wait_until_clickable(xpath).click()
--- pages/utils.py
+++ pages/utils.py
@@ -1,4 +1,41 @@
+import datetime
+import logging
 import time
+from time import sleep
+
+
+def wait_until_ok(timeout=5, period=0.25):
+    """Retries function until ok"""
+    logger = logging.getLogger("[WaitUntilOk]")
+
+    def decorator(original_function):
+
+        def wrapper(*args, **kwargs):
+            end_time = datetime.datetime.now() + datetime.timedelta(seconds=timeout)
+            while True:
+                try:
+                    return original_function(*args, **kwargs)
+                except Exception as err:
+                    if datetime.datetime.now() > end_time:
+                        logger.warning(f"Catch: {err}")
+                        raise err
+                    sleep(period)
+
+        return wrapper
+
+    return decorator
+
+
+def log_wrapper(func):
+    """Add log for method using doc string"""
+
+    def wrapper(*args, **kwargs):
+        log = logging.getLogger("[LogDecorator]")
+        result = func(*args, **kwargs)
+        log.info(f'{func.__doc__}')
+        return result
+
+    return wrapper
 
 
 class User:
