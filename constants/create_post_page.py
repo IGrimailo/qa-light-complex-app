c1d24a51aaa1b1d84b8502efe0666b24c4af99f9
IGrimaylo <igrimailo@corp.sendios.io>
2022-06-05 23:44:03 +0200

    Homework 23.05

--- constants/create_post_page.py
+++ constants/create_post_page.py
@@ -5,4 +5,6 @@ class CreatePostPageConstants:
     UPDATE_POST_BUTTON_XPATH = './/button[text()="Save Updates"]'
     POST_CREATE_SUCCESS_MESSAGE_XPATH = './/div[@class="alert alert-success text-center"]'
     POST_CREATE_SUCCESS_MESSAGE_TEXT = 'New post successfully created.'
-
+    TYPE_AVAILABILITY_XPATH = './/u'
+    TYPES_OF_AVAILABILITY = ["All Users", "One Person", "Group Message"]
+    SELECT_VALUE_XPATH = './/select/option[@value="{type}"]'
--- pages/create_post_page.py
+++ pages/create_post_page.py
@@ -19,7 +19,21 @@ class CreatePostPage(BasePage):
         self.click(xpath=self.constants.SAVE_POST_BUTTON_XPATH)
 
     @log_wrapper
+    def create_private_post(self, title: str, content: str) -> None:
+        """Create a new post using provided values"""
+        self.fill_field(xpath=self.constants.TITLE_FIELD_XPATH, value=title)
+        self.fill_field(xpath=self.constants.BODY_CONTENT_FIELD_XPATH, value=content)
+        self.click(xpath=self.constants.SELECT_VALUE_XPATH.format(type='One Person'))
+        self.click(xpath=self.constants.SAVE_POST_BUTTON_XPATH)
+
+    @log_wrapper
     def verify_message(self) -> None:
         """Verify success message after create a new post"""
         success_message = self.wait_until_displayed(xpath=self.constants.POST_CREATE_SUCCESS_MESSAGE_XPATH)
         assert success_message.text == self.constants.POST_CREATE_SUCCESS_MESSAGE_TEXT, 'Post is not created'
+
+    @log_wrapper
+    def verify_availability_message(self) -> None:
+        """Verify success message after create a new post"""
+        success_message = self.wait_until_displayed(xpath=self.constants.TYPE_AVAILABILITY_XPATH)
+        assert success_message.text == self.constants.TYPES_OF_AVAILABILITY[1], 'Post is not created'
